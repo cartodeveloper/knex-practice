82786a3e6ed59ecd2fa2caf051deaf4dcefd3db8
María Torrente <caritotorrente@gmail.com>
2020-12-09 12:32:09 -0500

    practice drills

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+INSERT INTO blogful_articles (title, date_published, content)
+ VALUES
+    ('Top 10 articles', '2016-01-16 12:00:00',       'This are the top ten articles list'),
+  ('Get the wine gold ',  '2016-05-01 15:00:00',       'Shape of Pooh'),
+  ('Believe in yourself',     '2017-02-22 12:00:00',       'UpTown Monk'),
+  ('Love is',      '2017-04-04 08:00:00',       'Despotato'),
+  ('Whatever ',  '2017-04-23 15:00:00',       'Despotato'),
+  ('Maybe', '2017-08-11 13:00:00',       'Cats that teach SQL'),
+  ('Sometime ',  '2017-12-09 17:00:00',       'Despotato'),
+  ('South',     '2018-01-24 19:00:00',       'Cats that teach SQL'),
+  ('West',      '2018-01-29 11:00:00',       'Man''s not torrid'),
+  ('Northeast', '2018-02-13 05:00:00',       'UpTown Monk'),
+  ('Midwest ',  '2018-03-13 09:00:00',       'Shape of Pooh'),
+  ('South',     '2018-03-31 13:00:00',       'UpTown Monk'),
+  ('Northeast', '2019-04-03 07:00:00',       'Despotato'),
+  ('West',      '2019-05-05 21:00:00',       'UpTown Monk')
+
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const { expect } = require("chai");
 const supertest = require("supertest");
 
 global.expect = expect;
-global.supertest = supertes;
+global.supertest = supertest;
